a9b86ad9f7de57d93823ea1dba9169c6f4e0bdfa
mansi singh <mansi123singh1234@gmail.com>
2024-11-18 08:21:20 +0530

    pattern

--- pattern/HalfDiamondStar.cpp
+++ pattern/HalfDiamondStar.cpp
@@ -4,19 +4,21 @@ using namespace std;
 int main()
 {
     int n; // 
-    cout << "Enter the value of n: ";
-    cin >> n; // 
-
-    for (int i = 1; i <= 2 * n - 1; i++)
-    {
-        int stars = i;
-        if (i > n)
-            stars = 2 * n - i;
-        for (int j = 1; j <= stars; j++)
-        {
-            cout << "* "; 
+    cout << "Enter the value of n: " <<endl;
+    cin >> n; 
+    for(int i=1; i<=n; i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
         }
-        cout << endl;
+        cout<<endl;
     }
+     for(int i=n-1; i>=1; i--){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
     return 0;
 }
