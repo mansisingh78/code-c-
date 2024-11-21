01285b241f303ebe0ea3f96cdb088ce4e1925a2f
mansi singh <mansi123singh1234@gmail.com>
2024-11-21 09:20:30 +0530

    using function pattern

---
+++ function/InvertedRightPyramid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+void printRightTriangle(int n) {
+    for (int i = n; i >= 1; i--) {       
+        for (int j = 1; j <= i; j++) {   
+            cout << "*";
+        }
+        cout << endl;                    
+    }
+}
+
+int main() {
+    int n;
+
+    cout << "Enter the number : ";
+    cin >> n;
+
+    
+    printRightTriangle(n);
+
+    return 0;
+}
