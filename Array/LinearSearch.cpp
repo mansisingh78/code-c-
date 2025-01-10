06e0721f47f147842e38fd7b7f4171fee9f48deb
mansi singh <mansi123singh1234@gmail.com>
2025-01-10 09:35:59 +0530

    arrays

--- Array/LinearSearch.cpp
+++ Array/LinearSearch.cpp
@@ -1,21 +1,31 @@
 #include<iostream>
 using namespace std;
 
-int linearSearch( int arr[],int sz, int target){
+bool Search( int arr[],int size, int key){
     
-    for(int i=0; i<sz; i++){
-         if(arr[i] == target){
-            return i;
+    for(int i=0; i<size; i++){
+         if(arr[i] == key){
+            return 1;
          }
     }
-    return -1;
+    return 0;
 }
 int main(){
-    int arr[] = {4,5,7,8,1,2,5};
-    int sz = 78;
-    int target = 5;
-    cout << linearSearch(arr, sz, target) <<endl;
-    return 0; 
+    int arr[10] = {4,5,7,8,1,2,5,-5,-2,-1};
+    
+    cout << "enter the element to search for" <<endl;
+    int key;
+    cin >> key;
+    bool found = Search(arr,10,key); 
+
+    if(found){
+        cout << "key is present"<< endl;
+    }
+    else{
+        cout<<"key is absent"<<endl;
+    }
+
+    return 0;
 
    
 }
--- Array/ReverseArr.cpp
+++ Array/ReverseArr.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 
-void reverseArray(int arr[],int sz){
-    int start = 0,end = sz-1;
+void reverseArray(int arr[],int n){
+    int start = 0,end = n-1;
 
     while(start < end){
         swap(arr[start],arr[end]);
@@ -10,16 +10,23 @@ void reverseArray(int arr[],int sz){
         end--;
     }
 }
+
+void printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout << arr[i]<<" ";
+    }
+    cout<< endl;
+}
 int main(){
-    int arr[] = {4,2,7,8,1,2,5};
-        int sz = 7;
+    int arr[7] = {4,2,7,8,1,2,5};
+    int brr[5] = {2,6,3,9,4};
+    
 
-        reverseArray(arr, sz);
+        reverseArray(arr, 7);
 
-        for(int i=0; i<sz;i++){
-            cout << arr[i] << " ";
-        }
-        cout<<endl;
+        printArray(arr,5);
+
+       
         return 0;
     
 
